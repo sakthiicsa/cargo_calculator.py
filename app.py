59bbe099a14f2fa00bad8006f490ddcb34b5756a
sakthiicsa <sakthiicsa@gmail.com>
2026-03-17 15:13:29 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ col1, col2 = st.columns([1, 4])
 
 with col1:
     st.image("Logo1.png", width=100)
-    st.image("Logo2.png", width=100)# your logo file
 
 with col2:
     st.title("International Clearing And Shipping Agency")
